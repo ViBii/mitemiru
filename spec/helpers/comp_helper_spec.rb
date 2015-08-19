a1f4f7dad1cac8aaef2422160ab6f6991395d7c0
yaginuuu <yaginuuu56@gmail.com>
2015-08-20 01:48:29 +0900

    テスト変更

--- spec/helpers/comp_helper_spec.rb
+++ spec/helpers/comp_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe CompHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+#RSpec.describe CompHelper, type: :helper do
+#  pending "add some examples to (or delete) #{__FILE__}"
+#end
