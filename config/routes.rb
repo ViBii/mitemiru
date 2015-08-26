463395fcf290df0c72d8c7d6ee333e0a7fe86475
Ryota Oki <r1118oboe@gmail.com>
2015-08-26 20:00:19 +0900

    Create redmine new page

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :projects
   resources :developers
+  resources :redmine
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
