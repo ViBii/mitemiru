c0118f3da0f1cd2de97c75f6f8b0c4b1dbaff2cc
Ryota Oki <r1118oboe@gmail.com>
2015-08-26 23:19:20 +0900

    Define redmine_key_params

--- app/controllers/redmine_keys_controller.rb
+++ app/controllers/redmine_keys_controller.rb
@@ -3,4 +3,21 @@ class RedmineKeysController < ApplicationController
   def new
     @redmine_key = RedmineKey.new
   end
+
+  # POST /redmine_keys/new
+  def create
+    @redmine_key = RedmineKey.new(redmine_key_params)
+    #respond_to do |format|
+      #format.html { render :new }
+    #end
+  end
+
+  def redmine_key_params
+    params.require(:redmine_keys).permit(
+      :url,
+      :login_name,
+      :password_digest,
+      :api_key
+    )
+  end
 end
