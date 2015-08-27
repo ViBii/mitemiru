f19923d65bcb1887b8a55dd6a1ef69f3d0677463
Ryota Oki <r1118oboe@gmail.com>
2015-08-27 16:31:45 +0900

    add form

--- app/controllers/ticket_repositories_controller.rb
+++ app/controllers/ticket_repositories_controller.rb
@@ -1,2 +1,12 @@
 class TicketRepositoriesController < ApplicationController
+  # GET /ticket_repositories/new
+  def new
+    @ticket_repository = TicketRepository.new
+  end
+
+  # POST /ticket_repositories/new
+  def index
+    @ticket_repository = TicketRepository.new
+    redirect_to '/ticket_repositories/new'
+  end
 end
