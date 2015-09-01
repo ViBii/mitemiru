8e302d9adec38ec0a268a841aa29b088ecce52fc
Ryota Oki <r1118oboe@gmail.com>
2015-09-01 19:02:17 +0900

    Have deletion Error of redmine_keys table

--- app/controllers/redmine_keys_controller.rb
+++ app/controllers/redmine_keys_controller.rb
@@ -1,4 +1,6 @@
 class RedmineKeysController < ApplicationController
+  before_action :set_redmine_key, only: [:show, :edit, :update, :destroy]
+  
   def index
     @redmine_key = RedmineKey.all
   end
@@ -13,10 +15,21 @@ class RedmineKeysController < ApplicationController
     @redmine_key = RedmineKey.new(redmine_key_params)
 
     if @redmine_key.save
-      redirect_to '/redmine_keys/new', notice: 'Successfully registered.'
+      redirect_to '/redmine_keys/', notice: 'Successfully registered.'
     else
-      redirect_to '/redmine_keys/new', status: 'Failed to register.'
+      format.html { redirect_to '/redmine_keys/new', status: 'Failed to register.' }
     end
+  end
+
+  def destroy
+    @redmine_key.destroy
+    respond_to do |format|
+      format.html { redirect_to redmine_keys_url, notice: 'Developer was successfully destroyed.' }
+    end
+  end
+
+  def set_redmine_key
+    @redmine_key = RedmineKey.find(params[:id])
   end
 
   def redmine_key_params
