17eab80a3432c1c4d4fcda83e92bcb0aee2b789f
Ryota Oki <r1118oboe@gmail.com>
2015-09-17 22:25:45 +0900

    Add sample graph

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,3 +9,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+#commit_counter
+Rails.application.config.assets.precompile += %w( commit_counter.js )
