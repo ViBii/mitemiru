ba13494f97b32d7cdb70f8676dab1260f6407dfb
Altairzym <zouyimin88@gmail.com>
2015-09-23 23:04:17 -0700

    Octokitを利用する仕組みに変更した

--- app/controllers/commit_counter_controller.rb
+++ app/controllers/commit_counter_controller.rb
@@ -4,23 +4,31 @@ class CommitCounterController < ApplicationController
   end
 
   def getcommits
-    @version_repo_id = 1
-    @repo_url = VersionRepository.find(@version_repo_id)[:url]
-    @version_repo = Hash.new
-    @version_repo[:url]=@repo_url.gsub(/https:\/\/github.com/,'https://api.github.com/repos')
+
+    #repo設定
+    githubRepo = "ViBii/mitemiru"
+
+    contributors = Octokit.contribs(githubRepo)
+
+    #@version_repo_id = 1
+    #@repo_url = VersionRepository.find(@version_repo_id)[:url]
+    #@version_repo = Hash.new
+    #@version_repo[:url]=@repo_url.gsub(/https:\/\/github.com/,'https://api.github.com/repos')
 
     # get total-commits data
-    req = RestClient::Request.execute method: :get, url: @version_repo[:url]+'/stats/contributors'
+    #req = RestClient::Request.execute method: :get, url: @version_repo[:url]+'/stats/contributors'
 
     # perse
-    hash = JSON.parse(req)
+    #hash = JSON.parse(req)
 
+    #hash.each do |json|
+    #  finalstr.concat(json['author']['login'] + ":" + json['total'].to_s + "<br>")
+    #end
     finalstr = ""
-    hash.each do |json|
-      # Rails.logger.info json.inspect
-      finalstr.concat(json['author']['login'] + ":" + json['total'].to_s + "<br>")
+    contributors.each do |contributor|
+      finalstr.concat(contributor['login'] + ":" + contributor['contributions'].to_s + "<br>")
     end
-
+    
     render :text => finalstr
 
   end
