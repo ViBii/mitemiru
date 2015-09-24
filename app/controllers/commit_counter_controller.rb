00160501dd7ab0bf9ce4be210fc7074950e17a73
Ryota Oki <r1118oboe@gmail.com>
2015-09-24 14:24:51 +0900

    Add data label on graph

--- app/controllers/commit_counter_controller.rb
+++ app/controllers/commit_counter_controller.rb
@@ -3,9 +3,15 @@ class CommitCounterController < ApplicationController
     #コミット数の集計処理を記載
 
     @commit_info = Hash.new
+    
     # The number of commits
-    @commit_info[:count] = 12
-
+    # Sample data
+    @commit_info[:count] = [12, 30]
     gon.commit_count = @commit_info[:count]
+
+    # Project ID
+    # Sample data
+    @commit_info[:project_id] = [1, 4]
+    gon.project_id = @commit_info[:project_id]
   end
 end
