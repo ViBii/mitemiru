762e17971eafa92a2085363090b51f61f583e9ac
Altairzym <zouyimin88@gmail.com>
2015-09-28 08:56:11 -0700

    点と点の間の線にコメント数字が表示できるように実装した

--- app/assets/javascripts/comments_counter_graph.js
+++ app/assets/javascripts/comments_counter_graph.js
@@ -20,9 +20,23 @@ force
 
 var link = svg.selectAll(".link")
     .data(graph.links)
-    .enter().append("line")
+    .enter().append("g")
     .attr("class", "link")
+    .append("line")
+    .attr("class", "link-line")
     .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+var linkText = svg.selectAll(".link")
+    .append("text")
+    .attr("class", "link-label")
+    .attr("font-family", "Arial, Helvetica, sans-serif")
+    .attr("fill", "Black")
+    .style("font", "normal 12px Arial")
+    .attr("dy", ".35em")
+    .attr("text-anchor", "middle")
+    .text(function(d) {
+        return d.value;
+    });
 
 var node = svg.selectAll(".node")
     .data(graph.nodes)
@@ -47,6 +61,14 @@ force.on("tick", function() {
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
+
+    linkText
+        .attr("x", function(d) {
+            return ((d.source.x + d.target.x)/2);
+        })
+        .attr("y", function(d) {
+            return ((d.source.y + d.target.y)/2);
+        });
 
     node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 });
