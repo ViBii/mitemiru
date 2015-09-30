e282c664c58c42e135bc792fd2c361f8697c9357
Altairzym <zouyimin88@gmail.com>
2015-09-30 00:34:49 -0700

    実績工数と予定工数の計算部分の作成

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -27,16 +27,15 @@ class PortfolioController < ApplicationController
   end
 
   def productivity
-    #***********************************************get ProductionCost
-
+    #*************************************Redmineアカウント情報、tracker情報の取得
     #redmine上のアカウント名
     @developer_name = "SYU"
     #redmine上のアカウントID
     developer_redmineId = nil
 
     #redmine上認証
-    redmineName = "zouyimin"
-    redminePW = "z13299928050"
+    redmineName = ENV['RedmineName']
+    redminePW = ENV['RedminePW']
 
     #redmine上の全てのアカウントを取得し、その中から該当開発者のIDをもらう
     redmine_url = 'http://vibi-redmine.herokuapp.com/projects/vibi'
@@ -58,47 +57,75 @@ class PortfolioController < ApplicationController
 
     # トラッカーHash
     @productivity_info = Hash.new
+    @productivity_info[:tracker] = []
 
-    # トラッカー名
-    @productivity_info[:tracker] = ['BUG', 'FEATURE', 'SUPPORT', 'DOCUMENT']
-    gon.tracker = @productivity_info[:tracker]
+    # トラッカー名の取得
+    tracker_req = RestClient::Request.execute method: :get, url: 'http://vibi-redmine.herokuapp.com/trackers.json', user: redmineName, password: redminePW
+    tracker_json = JSON.parse(tracker_req)
 
-    #実績工数Hash
-    estimated_hours = Hash.new
-    #実績工数Hash初期化
-    @productivity_info[:tracker].each do |tracker|
-      estimated_hours[tracker] = 0
+    tracker_json['trackers'].each do |tracker|
+      @productivity_info[:tracker].push(tracker['name'])
     end
 
+    gon.tracker = @productivity_info[:tracker]
+
+    #予定工数Hash
+    estimated_hours = Hash.new
+    #実績工数Hash
+    result_hours = Hash.new
+
+    #実績工数情報の取得
+    result_req = RestClient::Request.execute method: :get, url: redmine_url+'/time_entries.json', user: redmineName, password: redminePW
+    result_json = JSON.parse(result_req)
+
+    #工数Hash初期化
+    @productivity_info[:tracker].each do |tracker|
+      estimated_hours[tracker] = 0
+      result_hours[tracker] = 0
+    end
+
+    #*************************************************工数計算部分
     #予定工数の計算
     issues_json['issues'].each do |issue|
-      @productivity_info[:tracker].each do |tracker|
-        if issue['tracker']['name'] == tracker then
-          estimated_hours[tracker] = estimated_hours[tracker] + issue['estimated_hours']
+
+      #予定工数計算部分
+      if nil != issue['estimated_hours'] then
+        estimated_hours[issue['tracker']['name']] = estimated_hours[issue['tracker']['name']] + issue['estimated_hours']
+      end
+
+      #実績工数計算部分
+      result_json['time_entries'].each do |time_entry|
+        if issue['id'] == time_entry['issue']['id'] then
+          result_hours[issue['tracker']['name']] = result_hours[issue['tracker']['name']] + time_entry['hours']
         end
       end
 
     end
 
-    #実績工数Array
+    #予定工数Arrayの設定
     estimated_hours_result = []
     estimated_hours.each{|key, value|
       estimated_hours_result.push(value)
     }
-    puts estimated_hours_result
+    #実績工数Arrayの設定
+    result_hours_result = []
+    result_hours.each{|key, value|
+      result_hours_result.push(value)
+    }
 
+    puts result_hours_result
 
-    #******************************************************graph
+    #****************************************************graph
 
     # 実績工数
-    @productivity_info[:result] = [120, 56, 79, 12]
+    @productivity_info[:result] = result_hours_result
     gon.task_result = @productivity_info[:result]
 
     # 予定工数
-    @productivity_info[:estimate] = [100, 70, 70, 10]
+    @productivity_info[:estimate] = estimated_hours_result
     gon.task_estimate = @productivity_info[:estimate]
 
     # 開発者名
-    @productivity_info[:developer] = '玄葉 条士郎'
+    @productivity_info[:developer] = @developer_name
   end
 end
