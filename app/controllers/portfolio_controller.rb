42696b4b5ea4ac1cd3f8ef8746c5bc3fecba42c5
Altairzym <zouyimin88@gmail.com>
2015-09-29 17:06:06 -0700

    redmineAPIから開発者issue情報の取得

--- app/controllers/portfolio_controller.rb
+++ app/controllers/portfolio_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class PortfolioController < ApplicationController
   def index
   end
@@ -25,6 +27,37 @@ class PortfolioController < ApplicationController
   end
 
   def productivity
+    #**************************************************get issue Time
+
+    #redmine上のアカウント名
+    @developer_name = "SYU"
+    #redmine上のアカウントID
+    developer_redmineId = nil
+
+    #redmine上認証
+    redmineName = "zouyimin"
+    redminePW = "z13299928050"
+
+    #redmine上の全てのアカウントを取得し、その中から該当開発者のIDをもらう
+    memberships_url = 'http://vibi-redmine.herokuapp.com/projects/vibi'
+    memberships_req = RestClient::Request.execute method: :get, url: memberships_url+'/memberships.json', user: redmineName, password: redminePW
+
+    # perse
+    memberships_json = JSON.parse(memberships_req)
+
+    memberships_json['memberships'].each do |membership|
+      if membership['user']['name'] == @developer_name then
+        developer_redmineId = membership['user']['id']
+      end
+    end
+
+    #redmine上の該当開発者の全てのissue情報を取得する
+    issues_req = RestClient::Request.execute method: :get, url: memberships_url+'/issues.json?status_id=*&assigned_to_id='+ developer_redmineId.to_s, user: redmineName, password: redminePW
+
+    issues_json = JSON.parse(issues_req)
+
+    #puts issues_json
+    #******************************************************graph
     @productivity_info = Hash.new
 
     # トラッカー名
