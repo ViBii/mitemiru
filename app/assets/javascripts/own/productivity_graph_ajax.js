25a0933ac78d4fa9965ff062bd97f90a8a0d2052
Altairzym <zouyimin88@gmail.com>
2015-10-15 09:02:09 -0700

    jsファイルのインデントスペースの修正

--- app/assets/javascripts/own/productivity_graph_ajax.js
+++ app/assets/javascripts/own/productivity_graph_ajax.js
@@ -5,8 +5,7 @@ var costAjax = function() {
     dataType: "json",
     data: "",
     success: function(commit_data) {
-        //alert(commit_data.tracker);
-        create_productivity_graph(commit_data.tracker,commit_data.result_hours_result,commit_data.estimated_hours_result);
+      create_productivity_graph(commit_data.tracker,commit_data.result_hours_result,commit_data.estimated_hours_result);
     }
   })
   .done(function(commit_data) {
