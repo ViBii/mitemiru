b02ebe2b5093464409958952e2f8227387638f67
sjc1209 <yoshishi113@gmail.com>
2015-10-20 23:28:27 -0700

    change helper and index

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,16 +1,10 @@
 module ProjectsHelper
   def authen(project_id)
-    projects = Project.where(:id=>project_id)
-    projects.each do |pro|
-     version_id = pro.version_repository_id
-     redmin_id = pro.ticket_repository_id
-      if version_id == nil && redmin_id == nil
-        return false
-      else
+    project = Project.where(:id=>project_id)
+    return true if project.first.version_repository_id.present?
+    false
 
-          return true
-      end
-  end
+
   end
 end
 
