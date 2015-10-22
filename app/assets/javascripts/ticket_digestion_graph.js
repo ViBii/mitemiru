1ad972adcaf72ebcaa60d0c9480b54921d2869b4
Ryota Oki <r1118oboe@gmail.com>
2015-10-22 19:36:28 +0900

    Add flexible radius

--- app/assets/javascripts/ticket_digestion_graph.js
+++ app/assets/javascripts/ticket_digestion_graph.js
@@ -4,7 +4,7 @@ var create_ticket_digestion_graph = function(tracker,ticket_num,ticket_num_all){
   var ticket_num_all = ticket_num_all;
 
   var test_prospect_time = [20, 15, 8, 30];
-  var test_ressult_time = [25, 10, 8, 40];
+  var test_result_time = [25, 10, 8, 40];
 
   var base_color = ['#4f81bd', '#c0504d', '#9bbb59', '#8064a2', '#4bacc6', '#f79646'];
   var bright_color = ['#99b6d9', '#db9a98', '#c7d9a1', '#b4a4c8', '#98d0df', '#fbcda8'];
@@ -12,12 +12,18 @@ var create_ticket_digestion_graph = function(tracker,ticket_num,ticket_num_all){
 
   var width = 960,
       height = 500,
-      radius = Math.min(width, height) / 2;
+      base_radius = Math.min(width, height) / 3;
     
   var pros_arc = d3.svg.arc()
-              .outerRadius(radius)
+              .outerRadius(base_radius)
               .innerRadius(0);
-    
+
+  var result_arc = d3.svg.arc()
+                     .outerRadius(function(d, i) {
+                       return base_radius*(test_prospect_time[i]/test_result_time[i]);
+                     })
+                     .innerRadius(0);
+
   var pie = d3.layout.pie()
               .sort(null)
               .value(function(d) {
@@ -43,6 +49,17 @@ var create_ticket_digestion_graph = function(tracker,ticket_num,ticket_num_all){
           return bright_color[i];
         });
 
+  var result_g = svg.selectAll(".result")
+                  .data(pie(test_prospect_time))
+                  .enter()
+                  .append("g")
+                  .attr("class", "result");
+    
+  result_g.append("path")
+        .attr("d", result_arc)
+        .style("fill", function(d,i) {
+          return base_color[i];
+        });
 
 //  g.append("text")
 //      .attr("transform", function(d) {
