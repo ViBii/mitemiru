fe6ac2fc8ddeb9e7e6986ee58c7126714c6a8a89
yaginuuu <yaginuuu56@gmail.com>
2015-10-23 08:17:32 +0900

    プロジェクト登録フローひと通りできました.

--- app/models/github_key.rb
+++ app/models/github_key.rb
@@ -1,4 +1,3 @@
 class GithubKey < ActiveRecord::Base
-  belongs_to :version_repository
-  #has_and_belongs_to_many :users
+  has_and_belongs_to_many :users
 end
--- app/models/redmine_authority.rb
+++
@@ -1,2 +0,0 @@
-class RedmineAuthority < ActiveRecord::Base
-end
