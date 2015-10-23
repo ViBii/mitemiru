f19d59c0eecaa9b924371dc32243b9e9ee24d734
sjc1209 <yoshishi113@gmail.com>
2015-10-22 21:37:00 -0700

    change pagename

--- config/routes.rb
+++ config/routes.rb
@@ -24,10 +24,10 @@ Rails.application.routes.draw do
 
 
   resources :projects do
-    get '/authen_git' => 'projects#authen_git'
-    get '/authen_red' => 'projects#authen_red'
-    get '/add_git' => 'projects#add_git'
-    get '/add_red' => 'projects#add_red'
+    get '/authen_github' => 'projects#authen_github'
+    get '/authen_redmine' => 'projects#authen_redmine'
+    get '/add_github' => 'projects#add_github'
+    get '/add_redmine' => 'projects#add_redmine'
   end
 
   devise_for :users
