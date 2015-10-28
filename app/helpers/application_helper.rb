9506579d72afe4be5ace400fbc41a08c304524ce
yaginuuu <yaginuuu56@gmail.com>
2015-10-28 09:35:43 +0900

    システムユーザの権限があるデータのみを表示するようにしますた.(helperメソッド作成)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,6 @@
 module ApplicationHelper
+  # TODO: テスト記述しないと...
+
   def auth_git(project_id)
     project = Project.where(:id => project_id)
     return true if project.first.version_repository_id.present?
@@ -11,8 +13,29 @@ module ApplicationHelper
     false
   end
 
-  def did_current_user_make_this_project?(project)
-    true
+  def show_project?(project)
+    if current_user.redmine_keys.where(ticket_repository_id: project.ticket_repository_id).present?
+      true
+    elsif current_user.github_keys.where(version_repository_id: project.version_repository_id).present?
+      true
+    else
+      false
+    end
+  end
 
+  def show_developer?(developer)
+    show_developer = nil
+    developer.projects.each do |project|
+      if current_user.redmine_keys.where(ticket_repository_id: project.ticket_repository_id).present?
+        show_developer = true
+        break
+      elsif current_user.github_keys.where(version_repository_id: project.version_repository_id).present?
+        show_developer = true
+        break
+      else
+        show_developer = false
+      end
+    end
+    return show_developer
   end
 end
