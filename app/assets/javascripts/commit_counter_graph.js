b4f1beca292ac32c67548c3fa9cc8daebf561a16
Ryota Oki <r1118oboe@gmail.com>
2015-10-29 14:13:27 +0900

    Initialize svg area

--- app/assets/javascripts/commit_counter_graph.js
+++ app/assets/javascripts/commit_counter_graph.js
@@ -3,123 +3,36 @@ var create_commit_graph = function(all_commit,own_commit,developer_name){
     var developer_name = ['その他', developer_name];
     var color = ['#b1d7e8', '#006ab3'];
 
-    var topPadding = 50;
-    var leftPadding = 100;
-    var rightPadding = 20;
-    var w = 500+leftPadding+rightPadding;
-    var h = 400;
+    // 取得データの一覧
+    // developers: 開発者のリスト
+    // commit_count: 各開発者のコミット数
 
-    var barHeight = 100;
+    // 取得データサンプル(連携後に消去)
+    var developers = ['DeveloperA', 'DeveloperB', 'DeveloperC', 'DeveloperD', '玄葉      条士郎'];
+    var commit_count = [38, 56, 103, 11, 82];
 
-    var xScale = d3.scale.linear()
-        .domain([0, all_commit])
-        .range([0, 500])
-        .nice();
+    // グラフの色
+    var base_color = '#4f81bd';
+    var faint_color = '#749ccb';
 
-    var xAxis = d3.svg.axis()
-        .scale(xScale)
-        .orient("bottom");
+    // SVG領域の設定
+    var width = 960;
+    var height = 640;
+    var margin = {top: 0, right: 100, bottom: 0, left: 100};
 
+    // SVG領域の描画
     var svg = d3.select("body")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h);
+          .append("svg")
+          .attr("class", "commit_counter_graph")
+          .attr("width", width)
+          .attr("height", height);
 
-// x軸の表示
-    svg.append("g")
-        .attr({
-            class: "axis",
-            transform: "translate("+leftPadding+", 150)"
-        })
-        .call(xAxis);
-
-// 棒グラフの描画
-    svg.selectAll("rect")
-        .data(commit_count)
-        .enter()
-        .append("rect")
-        .attr("fill", function(d, i) {
-            return color[i];
-        })
-        .transition()
-        .duration(2000)
-        .each("start", function() {
-            d3.select(this).attr({
-                width: 0
-            })
-        })
-        .attr("x", leftPadding)
-        .attr("y", 40)
-        .attr("width", function(d) {
-            return xScale(d);
-        })
-        .attr("height", barHeight);
-
-// コミット数の表示
-    svg.selectAll("commit_count")
-        .data(commit_count)
-        .enter()
-        .append("text")
-        .attr("opacity", 0.0)
-        .text(function(d, i) {
-            if (i == 0) {
-                return d - commit_count[i+1];
-            } else {
-                return d;
-            }
-        })
-        .attr("x", function(d, i) {
-            if (i == 0) {
-                return leftPadding + xScale(commit_count[i+1]) + xScale(d - commit_count[i+1])/2;
-            } else {
-                return leftPadding + xScale(d)/2;
-            }
-        })
-        .attr("y", function(d) {
-            return 30 + barHeight/2;
-        })
-        .attr("text-anchor", "middle")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "20px")
-        .attr("fill", "white")
-        .transition()
-        .each("end", function() {
-            d3.select(this)
-                .transition()
-                .duration(2000)
-                .attr("opacity", 1.0)
-        });
-
-// 開発者名の表示
-    svg.selectAll("developer_name")
-        .data(developer_name)
-        .enter()
-        .append("text")
-        .attr("opacity", 0.0)
-        .text(function(d, i) {
-            return d;
-        })
-        .attr("x", function(d, i) {
-            if (i == 0) {
-                return leftPadding + xScale(commit_count[i+1]) + xScale(commit_count[i] - commit_count[i+1])/2;
-            } else {
-                return leftPadding + xScale(commit_count[i])/2;
-            }
-        })
-        .attr("y", function(d) {
-            return 30;
-        })
-        .attr("text-anchor", "middle")
-        .attr("font-family", "sans-serif")
-        .attr("font-size", "15px")
-        .attr("fill", "black")
-        .transition()
-        .each("end", function() {
-            d3.select(this)
-                .transition()
-                .duration(2000)
-                .attr("opacity", 1.0)
-        });
+    // SVG領域の確認
+    svg.append("text")
+      .text("Hello World")
+      .attr({
+        x: 150,
+        y: 150
+      })
+      .attr("font-size", "50px");
 }
-
-
