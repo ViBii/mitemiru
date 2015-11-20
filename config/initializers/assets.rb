3b00f15e074a0ea6be107cd07d8dc407d9dd14e4
yaginuuu <yaginuuu56@gmail.com>
2015-11-20 13:37:59 +0900

    プリコンパイル設定しました.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,14 +1,6 @@
-# Be sure to restart your server when you modify this file.
+Rails.application.config.assets.version = '1.6'
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.5'
-
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( lib/d3.min.js )
 Rails.application.config.assets.precompile += %w( own/comments_counter_graph.js )
 Rails.application.config.assets.precompile += %w( own/commit_counter_graph.js )
 Rails.application.config.assets.precompile += %w( own/productivity_graph.js )
